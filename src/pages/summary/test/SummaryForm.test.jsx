b7bd5cccf46ec390296e691713038290c695ad70
Juan Moreno <juanignacio.moreno@globant.com>
2022-10-21 00:28:41 -0300

    feat(SummaryFormTest): create the test

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,13 @@
-import { render } from '@testing-library/react'
-import { SummaryForm } from '../SummaryForm'
+import { fireEvent, render, screen } from "@testing-library/react"
+import SummaryForm from "../SummaryForm"
 
 test("checkbox is unchecked by default", () => {
   render(<SummaryForm />)
+  const myCheckbox = screen.getByRole("checkbox", { name: "my checkbox" })
+  expect(myCheckbox).not.toBeChecked()
+  const myButton = screen.getByRole("button", { name: "my button" })
+  expect(myButton).toBeDisabled()
+  fireEvent.click(myCheckbox)
+  expect(myCheckbox).toBeChecked()
+  expect(myButton).toBeEnabled()
 })
