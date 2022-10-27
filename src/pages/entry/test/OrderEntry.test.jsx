9f1e7870fcf74102478bcc23ebe23d9743e774c5
Juan Moreno <juanignacio.moreno@globant.com>
2022-10-27 14:33:09 -0300

    feat(orderEntry): add await for work better with findByRole

--- src/pages/entry/test/OrderEntry.test.jsx
+++ src/pages/entry/test/OrderEntry.test.jsx
@@ -1,9 +1,9 @@
-import { getByRole, render, screen } from "@testing-library/react"
+import { getByRole, render, screen, waitFor } from "@testing-library/react"
 import OrderEntry from "../OrderEntry"
 import { rest } from "msw"
 import { server } from "../../../mocks/server"
 
-test("handles error for scoops and toppings routes", async () => {
+test.only("handles error for scoops and toppings routes", async () => {
   server.resetHandlers(
     rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
       res(ctx.status(500))
@@ -13,8 +13,8 @@ test("handles error for scoops and toppings routes", async () => {
     )
   )
   render(<OrderEntry />)
-  const alerts = await screen.findAllByRole("alert", {
-    name: "An unexpected error ocurred. Please try again later"
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert")
+    expect(alerts).toHaveLength(2)
   })
-  expect(alerts).toHaveLength(2)
 })
