e54fdb0d04cc26e1c999dd89cf771a100d2f08df
Juan Moreno <juanignacio.moreno@globant.com>
2022-10-30 21:49:32 -0300

    a lot of changes

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,23 +1,33 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Row } from "react-bootstrap"
-import AlertBanner from "../common/AlertBanner"
-import ScoopOption from "./ScoopOption"
-import ToppingOption from "./ToppingOption"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
-const Options = ({ optionType }) => {
-  const [items, setItems] = useState([])
-  const [error, setError] = useState(false)
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
+
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => setError(true))
-  }, [optionType])
+      .catch((error) => setError(true));
+  }, [optionType]);
 
-  // TODO: replace `null` with ToppingOption when available
-  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption
+  if (error) {
+    // @ts-ignore
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent
@@ -25,14 +35,16 @@ const Options = ({ optionType }) => {
       name={item.name}
       imagePath={item.imagePath}
     />
-  ))
+  ));
 
   return (
     <>
-      {error && <AlertBanner />}
-      <Row> {optionItems}</Row>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
     </>
-  )
+  );
 }
-
-export default Options
--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,40 +1,48 @@
-import { useState } from "react"
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
-import OverlayTrigger from "react-bootstrap/OverlayTrigger"
-import Popover from "react-bootstrap/Popover"
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
-const popover = (
-  <Popover id="popover-basic">
-    <Popover.Body>No ice cream will actually be delivered</Popover.Body>
-  </Popover>
-)
+export default function SummaryForm({ setOrderPhase }) {
+  const [tcChecked, setTcChecked] = useState(false);
 
-const SummaryForm = () => {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // pass along to the next phase.
+    // The next page will handle submitting order from context.
+    setOrderPhase("completed");
+  }
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
       I agree to
       <OverlayTrigger placement="right" overlay={popover}>
-        <span style={{ color: "blue" }}>Terms and conditions</span>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
       </OverlayTrigger>
     </span>
-  )
+  );
 
-  const [tcChecked, setTcChecked] = useState(true)
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId="terms-and-conditions">
         <Form.Check
           type="checkbox"
-          onChange={(e) => setTcChecked(!e.target.checked)}
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
           label={checkboxLabel}
         />
       </Form.Group>
-      <Button variant="primary" type="submit" disabled={tcChecked}>
-        Confirm Order
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
       </Button>
     </Form>
-  )
+  );
 }
-
-export default SummaryForm
